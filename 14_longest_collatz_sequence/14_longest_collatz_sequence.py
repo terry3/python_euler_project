27f651f1e98116d07c82a4ba0133c065864c7633
terry3 <terryr3rd@yeah.net>
2015-07-02 17:36:39 +0800

    1.增加time的decorator。

--- 14_longest_collatz_sequence/14_longest_collatz_sequence.py
+++ 14_longest_collatz_sequence/14_longest_collatz_sequence.py
@@ -17,17 +17,9 @@ NOTE: Once the chain starts the terms are allowed to go above one million.
 
 HASH TABLE!!!
 """
-
-def exec_time(func):
-    import time
-    def newFunc(*args, **args2):
-        t0 = time.time()
-        print "%s, {%s} start" % (time.strftime("%X", time.localtime()), func.__name__)
-        back = func(*args, **args2)
-        print "%s, {%s} end" % (time.strftime("%X", time.localtime()), func.__name__)
-        print "%.3fs taken for {%s}" % (time.time() - t0, func.__name__)
-        return back
-    return newFunc
+import sys
+sys.path.append("..")
+from t_base import exec_time
 
 class Collatz_seq:
     def __init__(self):
---
+++ t_base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+just base.
+"""
+
+def exec_time(func):
+    import time
+    def new_func(*args, **kwargs):
+        begin = time.time()
+        back = func(*args, **kwargs)
+        print "%.3fs taken for [%s]" % (time.time() - begin, func.__name__)
+        return back
+    return new_func
+
