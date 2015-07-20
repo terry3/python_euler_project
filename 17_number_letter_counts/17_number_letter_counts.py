3aba5af9bd542b66513479fe1cc7a13588afed7f
terry3 <terryr3rd@yeah.net>
2015-07-20 20:48:10 +0800

    commit 17.

--- 17_number_letter_counts/17_number_letter_counts.py
+++ 17_number_letter_counts/17_number_letter_counts.py
@@ -8,13 +8,43 @@ If all the numbers from 1 to 1000 (one thousand) inclusive were written out in w
 NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
 
 one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty
+
 """
 
+digit_list =["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
 
-print "pu~~~"
+digit_ten_list = ["twenty", "thirty", "forty", "fifty", "sixty","seventy", "eighty", "ninety"]
 
+hundred_name = "hundred"
+dash_name = "and"
 
+def calc_digit_char_count():
+    t_sum = 0
+    t_index = 1
+    t_tens = 2
+    t_unit = 0
+    t_handred = 0
+    for var in digit_list:      # add 1-19
+        t_sum += len(var)
+    t_index = len(digit_list)
+    for var in range(t_index, 1000):
+        if t_unit > 9:
+            t_unit = 0
+            t_tens += 1
+        if t_tens > 9:
+            t_tens = 0
+            t_handred += 1
+        if t_handred > 9:
+            t_handred = 0
+        # reset t_*
+        if t_unit > 0:
+            t_sum += len(digit_list[t_unit - 1])
+        if t_tens > 0:
+            t_sum += len(digit_ten_list[t_tens - 2])
+        if t_handred > 0:
+            t_sum += len(digit_list[t_handred - 1]) + len(hundred_name) + len(dash_name)
+        t_unit += 1
+    return t_sum + len("one thousand")
 
-
-
-
+print "wrong answer."
+print calc_digit_char_count()
